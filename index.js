d572b4952b5258f3ea79d560389f5eab5f69c336
hinanaz <hinanaz4123@gmail.com>
2024-04-12 13:23:08 +0500

    tweak the text and added console at the end

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter firstNumber",
+        type: "number",
+        name: "firstNumber" },
+    { message: "Enter secondNumber",
+        type: "number",
+        name: "secondNumber" },
+    { message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtruction", "Multiplication", "Division"], },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtruction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
